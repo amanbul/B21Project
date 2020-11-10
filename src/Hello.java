0ae7960ab4b583e2d476f36266ec57b319de823f
Amannisa Sawuti <asawutiwupuer.15@gmail.com>
2020-11-10 12:49:08 -0500

    added some changes

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hey, you are going fast");
+        // making more commit, do I share again?
     }
 }
